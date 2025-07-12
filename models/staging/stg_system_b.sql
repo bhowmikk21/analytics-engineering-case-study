f0ab0f62458f8b0ad6e5da5c2a08a8ffcd36b3ad
Kausik Bhowmik <kausik.kb.bhowmik@gmail.com>
2025-07-12 16:03:39 +0530

    Add staging model for system A

---
+++ models/staging/stg_system_b.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['"b"', 'ref']) }} as company_id,
+    'B' AS system_name,
+    name AS company_name_raw,
+    ref AS company_ref,
+    status AS status_summary,
+    CAST(m1 AS INTEGER) AS metric_1,
+    CAST(m2 AS FLOAT) AS metric_2,
+    CAST(m3 AS INTEGER) AS metric_3,
+    CAST(m4 AS INTEGER) AS metric_4,
+    CAST(m5 AS FLOAT) AS metric_5,
+    CURRENT_DATE AS report_date
+FROM {{ source('report', 'system_b') }};
