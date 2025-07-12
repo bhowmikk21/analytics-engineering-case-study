a35061e2480ed60abb24320540bedda739850106
Kausik Bhowmik <kausik.kb.bhowmik@gmail.com>
2025-07-12 15:50:16 +0530

    Add staging model for system A

---
+++ models/staging/stg_system_a.sql
@@ -0,0 +1,13 @@
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['"A"', 'ref']) }} as company_id,
+    'A' AS system_name,
+    name AS company_name_raw,
+    ref AS company_ref,
+    status AS status_summary,
+    CAST(m1 AS INTEGER) AS metric_1,
+    CAST(m2 AS FLOAT) AS metric_2,
+    CAST(m3 AS INTEGER) AS metric_3,
+    CAST(m4 AS INTEGER) AS metric_4,
+    CAST(m5 AS FLOAT) AS metric_5,
+    CURRENT_DATE AS report_date
+FROM {{ source('system_a', 'report') }};
