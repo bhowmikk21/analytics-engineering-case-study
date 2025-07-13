a2c368314ce08e5cc2a7805f418a092a61eb7473
Kausik Bhowmik <kausik.kb.bhowmik@gmail.com>
2025-07-14 00:06:40 +0530

    Add staging tables

--- models/intermediate/int_name_mapping.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    u.*,
-    m.canonical_company_id,
-    m.mapped_name AS canonical_company_name
-FROM {{ ref('stg_all_sources_union') }} u
-LEFT JOIN {{ ref('dim_company_mapping') }} m
-  ON u.system_name = m.system_name AND u.company_ref = m.company_ref;
---
+++ models/staging/stg_multi_source_heirarchy.sql
@@ -0,0 +1,14 @@
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['parent_system', 'parent_referance']) }} as parent_id,
+    parent_system,
+    parent_referance,
+    {{ dbt_utils.generate_surrogate_key(['child_system', 'child_referance']) }} as child_id,
+    child_system,
+    child_referance
+FROM {{ source('heirarchy', 'multi_source') }};
